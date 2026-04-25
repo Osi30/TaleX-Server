bfe5ffd60c13029373be421cd235a99de6ff3422
Osi30 <nthanhn2491@gmail.com>
2026-04-25 10:38:43 +0700

    [INIT] Init commit with Apache 2.0 License

---
+++ src/main/java/com/talex/server/TalexServerApplication.java
@@ -0,0 +1,13 @@
+package com.talex.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalexServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalexServerApplication.class, args);
+	}
+
+}
