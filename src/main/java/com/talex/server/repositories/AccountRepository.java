cd73ed28c7dc9dcad510e2f4215b86860ddb4555
Subzerozdev <khanhtrantuc2004@gmail.com>
2026-06-01 18:40:47 +0700

    add JPA Repositories for auth entities

---
+++ src/main/java/com/talex/server/repositories/AccountRepository.java
@@ -0,0 +1,20 @@
+package com.talex.server.repositories;
+
+import com.talex.server.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    Optional<Account> findByEmail(String email);
+
+    Optional<Account> findByGoogleSubId(String googleSubId);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+}
