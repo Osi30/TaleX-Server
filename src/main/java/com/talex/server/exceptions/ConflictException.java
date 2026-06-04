cec9eba912ff98b65407b73483e84832aaf7bd66
Subzerozdev <khanhtrantuc2004@gmail.com>
2026-06-04 22:36:18 +0700

    [REFACTOR] Remove unused simple exception classes

--- src/main/java/com/talex/server/exceptions/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.talex.server.exceptions;
-
-public class ConflictException extends RuntimeException {
-
-    public ConflictException(String message) {
-        super(message);
-    }
-}
