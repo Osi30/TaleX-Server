6aac52eaa2585c69aa6e1eeb54374b25ea140124
Subzerozdev <khanhtrantuc2004@gmail.com>
2026-06-06 14:59:37 +0700

    [FEAT] Add QuestDB time-series database for analytics

---
+++ src/main/java/com/talex/server/configs/QuestDbConfig.java
@@ -0,0 +1,40 @@
+package com.talex.server.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class QuestDbConfig {
+
+    @Value("${questdb.host}")
+    private String host;
+
+    @Value("${questdb.port}")
+    private int port;
+
+    @Value("${questdb.username}")
+    private String username;
+
+    @Value("${questdb.password}")
+    private String password;
+
+    @Bean(name = "questDbDataSource")
+    public DataSource questDbDataSource() {
+        var dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://" + host + ":" + port + "/qdb");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean(name = "questDbJdbcTemplate")
+    public JdbcTemplate questDbJdbcTemplate() {
+        return new JdbcTemplate(questDbDataSource());
+    }
+}
