e6a56f0635d6f49595fd4f82401e2e608b589f67
inkB12 <ttinhbao@gmail.com>
2026-09-03 14:13:41 +0700

    [FIX] Add NoArgsConstructor to AdServeResponseDto

--- src/main/java/com/talex/server/dtos/responses/ads/AdServeResponseDto.java
+++ src/main/java/com/talex/server/dtos/responses/ads/AdServeResponseDto.java
@@ -1,11 +1,15 @@
 package com.talex.server.dtos.responses.ads;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdServeResponseDto {
     private UUID campaignId;
     private String mediaUrl;
